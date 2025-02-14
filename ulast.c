32428c8db7ea1eea805e39182bc259a82667d39e
mjpinard <mpinard16cchs@gmail.com>
2025-02-14 12:10:15 -0500

    adding flags in command line

--- ulast.c
+++ ulast.c
@@ -1,13 +1,15 @@
-#include	<stdio.h>
-#include	<unistd.h>
-#include	<sys/types.h>
-#include	<utmp.h>
-#include	<fcntl.h>
-#include	<time.h>
-#include	<stdlib.h>
-#include "dumputmp.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <utmp.h>
+#include <fcntl.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
 /*
- *	read from utmp file the login records
+ *	who version 2		- read /var/run/utmp and list info therein
+ * 				- surpresses empty records
+ *				- formats time nicely
  */
 
 /* UTMP_FILE is a symbol defined in utmp.h */
@@ -15,60 +17,99 @@
 /*       otherwise, program defaults to NUMDATE (1978-02-05)		*/
 
 #define TEXTDATE
-#ifndef	DATE_FMT
-   #   ifdef	TEXTDATE
-#     define	DATE_FMT	"%b %e %H:%M"		/* text format	*/
-#   else
-#     define	DATE_FMT	"%Y-%m-%d %H:%M"	/* the default	*/
-#   endif
+#ifndef DATE_FMT
+#ifdef TEXTDATE
+#define DATE_FMT "%b %e %H:%M" /* text format	*/
+#else
+#define DATE_FMT "%Y-%m-%d %H:%M" /* the default	*/
+#endif
 #endif
 
-void	show_info(struct utmp *);
+void show_info(struct utmp *);
+void read_wtmp_file(char *info);
 
 int main(int ac, char *av[])
 {
-	struct utmp	utbuf;		/* read info into here */
-	int		utmpfd;		/* read from this descriptor */
-	char		*info = ( ac > 1 ? av[1] : WTMP_FILE); //hardcode at first
-	ssize_t bytes_read;
-	
-    if ( (utmpfd = open( info, O_RDONLY )) == -1 ){
-		perror(info);
-		exit(1);
-	}
+    char *info = WTMP_FILE;
+    const char *username = "dce-lib215";
+    int e_flag = 0;
 
-    //Move file pointer to the end of the file
-	
-	if(lseek(utmpfd,0,SEEK_END)==-1){
-		perror("lseek");
-		close(utmpfd);
-		exit(EXIT_FAILURE);
-	}
-
-	//Read the file backwards one struct at a time - start at begining of last struct
-	while((bytes_read = lseek(utmpfd, -sizeof(utbuf),SEEK_CUR))!=-1){
-		
-		//get the first record and check that was successfully read into the buffer
-		if(read(utmpfd,&utbuf,sizeof(utbuf))!=sizeof(utbuf)){
-			perror("read");
-			close(utmpfd);
-			exit(EXIT_FAILURE);
-		}
-
-		//display login info
-		show_utrec( &utbuf );
-
-		//move file pointer back
-		if(lseek(utmpfd,-sizeof(utbuf),SEEK_CUR)==-1){
-			perror("lseek");
-			close(utmpfd);
-			exit(EXIT_FAILURE);
+    //process args
+    for(int i = 1; i< ac; i++){
+		if(strcmp(av[i], "-f")== 0 && i + 1 <ac){
+			info = av[i+1];
+			i++; //skip next arg bc it is the filepath
+		}else if(strcmp(av[i], "-e")== 0){
+			e_flag = 1;
+		}else{
+			username = av[i];
 		}
 	}
+    printf("username is: %s. \n",username);
+    printf("filename is: %s. \n",info);
+    printf("eflag is: %d. \n", e_flag);
 
-	close( utmpfd );
-	return 0;	
+
+    read_wtmp_file(info);
+
+    return 0;
 }
+
+
+void read_wtmp_file(char *info){
+    struct utmp utbuf; /* read info into here */
+    int utmpfd;        /* read from this descriptor */
+    ssize_t bytes_read;
+
+    if ((utmpfd = open(info, O_RDONLY)) == -1)
+    {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+
+    // Move file pointer to the end of the file
+    if (lseek(utmpfd, 0, SEEK_END) == -1)
+    {
+        perror("lseek");
+        close(utmpfd);
+        exit(EXIT_FAILURE);
+    }
+    // Read the file backwards one struct at a time - start at begining of last struct
+    while ((bytes_read = lseek(utmpfd, -sizeof(utbuf), SEEK_CUR)) != -1)
+    {
+
+        // get the first record and check that was successfully read into the buffer
+        if (read(utmpfd, &utbuf, sizeof(utbuf)) != sizeof(utbuf))
+        {
+            perror("read");
+            close(utmpfd);
+            exit(EXIT_FAILURE);
+        }
+
+        // display login info
+        show_info(&utbuf);
+
+        // if(strcmp(utbuf.ut_name, username)==0){
+        // 	//this is your correct user
+        // 	printf("this is your user)");
+        // 	printf("\n");
+        // }
+
+        // move file pointer back
+        if (lseek(utmpfd, -sizeof(utbuf), SEEK_CUR) == -1)
+        {
+            perror("lseek");
+            close(utmpfd);
+            exit(EXIT_FAILURE);
+        }
+    }
+    // while (read(utmpfd, &utbuf, sizeof(utbuf)) == sizeof(utbuf))
+    //     show_info(&utbuf);
+    close(utmpfd);
+}
+
+
+
 
 /*
  *	show info()
@@ -76,35 +117,36 @@ int main(int ac, char *av[])
  *			in human readable form
  *			* displays nothing if record has no user name
  */
-void show_info( struct utmp *utbufp )
+void show_info(struct utmp *utbufp)
 {
-	void showtime(time_t, char *);
+    void showtime(time_t, char *);
 
-	if ( utbufp->ut_type != USER_PROCESS )
-		return;
+    if (utbufp->ut_type != USER_PROCESS)
+        return;
 
-	printf("%-8.32s", utbufp->ut_name);		/* the logname	*/
-		/* better: "%-8.*s",UT_NAMESIZE,utbufp->ut_name) */
-	printf(" ");					/* a space	*/
- 	printf("%-12.12s", utbufp->ut_id);		/* the tty	*/
-	printf(" ");					/* a space	*/
-	showtime( utbufp->ut_time , DATE_FMT);		/* display time	*/
-	printf(" (%.256s)", utbufp->ut_host);	/*    show host	*/
-	printf("\n");					/* newline	*/
+    printf("%-8.32s", utbufp->ut_name);       /* the logname	*/
+                                              /* better: "%-8.*s",UT_NAMESIZE,utbufp->ut_name) */
+    printf(" ");                              /* a space	*/
+    printf("%-12.12s", utbufp->ut_line);      /* the tty	*/
+    printf(" ");                              /* a space	*/
+    showtime(utbufp->ut_time, DATE_FMT);      /* display time	*/
+    if (utbufp->ut_host[0] != '\0')           /* if not ""	*/
+        printf(" (%.256s)", utbufp->ut_host); /*    show host	*/
+    printf("\n");                             /* newline	*/
 }
 
-#define	MAXDATELEN	100
+#define MAXDATELEN 100
 
-void showtime( time_t timeval , char *fmt )
+void showtime(time_t timeval, char *fmt)
 /*
  * displays time in a format fit for human consumption.
  * Uses localtime to convert the timeval into a struct of elements
  * (see localtime(3)) and uses strftime to format the data
  */
 {
-	char	result[MAXDATELEN];
+    char result[MAXDATELEN];
 
-	struct tm *tp = localtime(&timeval);		/* convert time	*/
-	strftime(result, MAXDATELEN, fmt, tp);		/* format it	*/
-	fputs(result, stdout);
+    struct tm *tp = localtime(&timeval);   /* convert time	*/
+    strftime(result, MAXDATELEN, fmt, tp); /* format it	*/
+    fputs(result, stdout);
 }
